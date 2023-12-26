c3abc5ac0a468a7299c2a14a6ce341e3189cd2b3
fugu133 <fugu0220@gmail.com>
2023-12-26 13:32:45 +0900

    誤り修正

---
+++ GeoAtmos/Core.hpp
@@ -0,0 +1,13 @@
+/**
+ * @file Core.hpp
+ * @author Kaiji Takeuchi
+ * @brief 
+ * @version 0.1
+ * @date 2023-12-26
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#include "src/Nrlmsise00.hpp"
+#include "src/Essential.hpp"
